7f196bb79eca9dd4f65090741c21578d781f1554
TSdnl13 <daniel.int@outlook.com>
2022-03-24 23:54:34 -0500

    change data tipe attributes

---
+++ src/main/java/models/Cuenta.java
@@ -0,0 +1,34 @@
+
+package models;
+
+public class Cuenta {
+    
+    private String numeroCuenta;
+    private double monto;
+    private int idCliente;
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+  
+}
---
+++ src/main/java/models/Operacion.java
@@ -0,0 +1,54 @@
+
+package models;
+
+import java.time.LocalDateTime;
+
+public class Operacion {
+    
+    private int id;
+    private char tipo;
+    private double monto;
+    private LocalDateTime fecha;
+    private String numeroCuenta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+    
+}
