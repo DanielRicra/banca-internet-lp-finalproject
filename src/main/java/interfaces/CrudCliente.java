d72addbda5aec61d96b5bca33f41f0921fe2a3cc
TSdnl13 <daniel.int@outlook.com>
2022-03-26 14:01:12 -0500

    some fixes

--- src/main/java/interfaces/CrudCliente.java
+++ src/main/java/interfaces/CrudCliente.java
@@ -7,6 +7,7 @@ import models.Cliente;
 public interface CrudCliente {
     public List<Cliente> getAll();
     public Cliente getCliente(int id);
+    public Cliente getClienteByDNI(String dni);
     public boolean modificar(Cliente cliente);
     public boolean save(Cliente cliente);
     public boolean eliminar(int id);
